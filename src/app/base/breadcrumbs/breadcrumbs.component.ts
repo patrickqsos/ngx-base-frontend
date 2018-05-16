747009379575816c00d7632b0927ba0693032069
Patricio Quispe Sosa <patricio.quispe@segip.gob.bo>
2018-05-16 08:56:36 -0400

    Se agregó el componente para mostrar migas de pan

---
+++ src/app/base/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { LangService } from '../../shared/services/lang.service';
+import { BaseComponent } from '../../shared/base.component';
+import { AuthService } from '../../shared/services/auth.service';
+import { ContextoService } from '../../shared/services/contexto.service';
+
+/**
+ * Componente para manejar las migas de pan.
+ * 
+ * @export
+ * @class BreadcrumbsComponent
+ * @extends {BaseComponent}
+ * @implements {OnInit}
+ */
+@Component({
+    selector: 'app-breadcrumbs',
+    templateUrl: './breadcrumbs.component.html'
+})
+export class BreadcrumbsComponent extends BaseComponent implements OnInit {
+    
+    /**
+     * Creates an instance of BreadcrumbsComponent.
+     * @param {LangService} langService 
+     * @param {AuthService} authService 
+     * @param {ContextoService} contextoService 
+     * @memberof BreadcrumbsComponent
+     */
+    constructor(
+        public langService: LangService,
+        public authService: AuthService,
+        public contextoService: ContextoService,
+        
+    ) { 
+        super();
+    }
+
+    /**
+     * Métotod que se ejecuta cuando se hace click en un item del array de migas de pan.
+     * 
+     * @param {any} pItem Item clickeado.
+     * @param {any} pindex Index dle item clickeado.
+     * @memberof BreadcrumbsComponent
+     */
+    onBreadClick(pItem, pindex) {
+        // Carga los recursos hijos del elemento clickeado.
+        this.contextoService.listaMenu = pItem.RecursosHijos;
+        // Remuevo los items que esten despues del item clickeado en el array de migas de pan. 
+        this.contextoService.breadCrumbs.length = pindex + 1;
+    }
+
+    /**
+     * Método que se ejecuta cuando se hace click en el boton inicio.
+     * 
+     * @memberof BreadcrumbsComponent
+     */
+    onHomeClick() {
+        // Vacia el array de migas de pan.
+        this.contextoService.breadCrumbs = [];
+        // Carga el menu inicial.
+        this.contextoService.listaMenu = this.contextoService.getListaSchemas();
+    }
+
+    /**
+     * Hook on init del componente.
+     * 
+     * @memberof BreadcrumbsComponent
+     */
+    ngOnInit(): void { }
+}
