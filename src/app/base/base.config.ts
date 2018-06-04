6ab13e1e2542185899306e6a9e6956484085e974
Patricio Quispe Sosa <patricio.quispe@segip.gob.bo>
2018-06-04 18:20:46 -0400

    Se actualizó los configs para los entornos dev, analisis y test

--- src/app/base/base.config.ts
+++ src/app/base/base.config.ts
@@ -1,5 +1,5 @@
 // Objeto que contiene datos de la aplicacion.
 export const baseConfig = {
     version: '2.0.0',
-    nombreSistema: 'Sistema base segip v2'
+    nombreSistema: 'Sistema base segip'
 };
