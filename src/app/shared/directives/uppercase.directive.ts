50d8e00266e9f0a37614fb80f1139dc156a937eb
Patricio Quispe Sosa <patricio.quispe@segip.gob.bo>
2018-08-01 10:59:02 -0400

    Se corrigió la directiva shUppercase para que tambien haga el cambio al value del control

--- src/app/shared/directives/uppercase.directive.ts
+++ src/app/shared/directives/uppercase.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, AfterViewInit, ElementRef } from '@angular/core';
+import { Directive, AfterViewInit, ElementRef, HostListener, Output, EventEmitter, Renderer2, forwardRef } from '@angular/core';
+import { DefaultValueAccessor, NG_VALUE_ACCESSOR } from '../../../../node_modules/@angular/forms';
+
+const UPPERCASE_VALUE_ACCESSOR = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => UppercaseDirective),
+    multi: true
+};
 
 /**
  * Directiva para hacer que el value de un input sea en mayusculas.
@@ -8,16 +15,21 @@ import { Directive, AfterViewInit, ElementRef } from '@angular/core';
  * @implements {AfterViewInit}
  */
 @Directive({
-    selector: '[shUppercase]'
+    selector: '[shUppercase]',
+    providers: [
+        UPPERCASE_VALUE_ACCESSOR
+    ]
 })
-export class UppercaseDirective implements AfterViewInit {
+export class UppercaseDirective extends DefaultValueAccessor implements AfterViewInit  {
 
     /**
      *Creates an instance of UppercaseDirective.
      * @param {ElementRef} elementRef
      * @memberof UppercaseDirective
      */
-    constructor(public elementRef: ElementRef) {}
+    constructor(renderer: Renderer2, public elementRef: ElementRef) {
+        super(renderer, elementRef, false);
+    }
 
     /**
      * Hook AfterViewInit.
@@ -26,5 +38,40 @@ export class UppercaseDirective implements AfterViewInit {
      */
     ngAfterViewInit(): void {
         this.elementRef.nativeElement.style.textTransform = 'uppercase';
+    }
+
+    /**
+     * HostListener para detectar el evento input y hacer la transformación a uppercase.
+     *
+     * @param {*} $event
+     * @memberof UppercaseDirective
+     */
+    @HostListener('input', ['$event']) onInputChange($event): void {
+        const transformed = this.transformValue($event.target.value);
+        super.writeValue(transformed);
+        this.onChange(transformed);
+    }
+
+    /**
+     * HostListener para detectar el evento blur.
+     *
+     * @param {*} $event
+     * @memberof UppercaseDirective
+     */
+    @HostListener('blur') onBlur(): void {
+        this.onTouched();
+    }
+
+    /**
+     * Método que trasnforma el valor.
+     *
+     * @private
+     * @param {*} value
+     * @returns {*}
+     * @memberof UppercaseDirective
+     */
+    private transformValue(value: any): any {
+        const result = value && typeof value === 'string' ? value.toUpperCase() : value;
+        return result;
     }
 }
