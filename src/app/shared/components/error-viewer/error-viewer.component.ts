767c7fa1d840d8b2b312af3e1faf95c361b418d9
Patricio Quispe Sosa <patricio.quispe@segip.gob.bo>
2018-08-23 09:25:24 -0400

    Se corrigió el componente error viewer para ajustar a los cambios visuales. Closes #46

--- src/app/shared/components/error-viewer/error-viewer.component.ts
+++ src/app/shared/components/error-viewer/error-viewer.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Resultado } from '../../../shared/models/resultado.model';
 import { LangService } from '../../../shared/services/lang.service';
 import { BaseComponent } from '../../../shared/base.component';
 
+/**
+ * Componente para mostrar errores.
+ *
+ * @export
+ * @class ErrorViewerComponent
+ * @extends {BaseComponent}
+ */
 @Component({
     selector: 'shared-error-viewer',
     templateUrl: './error-viewer.component.html'
 })
 export class ErrorViewerComponent extends BaseComponent {
+
+    /**
+     * Creates an instance of ErrorViewerComponent.
+     * @param {MatDialogRef<ErrorViewerComponent>} dialogRef
+     * @param {Resultado} data
+     * @param {LangService} langService
+     * @memberof ErrorViewerComponent
+     */
     constructor(
         public dialogRef: MatDialogRef<ErrorViewerComponent>,
         @Inject(MAT_DIALOG_DATA) public data: Resultado,
         public langService: LangService
     ) {
         super();
-    }
-
-    onClose(): void {
-        this.dialogRef.close();
     }
 }
